d095950fcfdd386644aed4d8638ce8f44f67127b
MaNnu81 <mannuspamm@gmail.com>
2025-03-10 13:28:17 +0100

    gf

---
+++ regex.js
@@ -0,0 +1,10 @@
+const teststr = "Mi chiamo pippo, ho 7 fratelli, mi piace il numero 5 e vado spesso al parco che dista 8 km da casa mia";
+
+const rgx = /\d+/g;
+
+// console.log(rgx.test(teststr));
+
+// console.log(teststr.match(rgx));
+
+// console.log(rgx.exec(teststr));
+
